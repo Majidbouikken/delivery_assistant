285eac803aa845b4326afd3929bd999a291d134c
MajidMpressive <bouikkenmajid@gmail.com>
2021-04-04 21:37:34 -0700

    updating endpoints (hopefully final version)

--- app/src/main/java/com/example/deliveryassistant/Endpoint.kt
+++ app/src/main/java/com/example/deliveryassistant/Endpoint.kt
@@ -2,24 +2,37 @@ package com.example.deliveryassistant
 
 import com.example.deliveryassistant.models.Order
 import com.example.deliveryassistant.models.Product
+import com.example.deliveryassistant.models.User
 import retrofit2.Call
 import retrofit2.http.*
+import java.nio.file.attribute.UserPrincipalLookupService
 
 interface Endpoint {
-    @GET("getorders/{user_id}")
+    // 1 Authentication endpoint
+    @GET("login/{email}/{password}")
+    fun login(@Path("email") name: String, @Path("password") pass: String): Call<User>
+
+    // 2 Endpoints to display orders and products
+    @GET("getOrders/{user_id}")
     fun getOrders(@Path("user_id") user_id: Int): Call<List<Order>>
 
-    @GET("getorders/{order_id}")
+    @GET("getProducts/{order_id}")
     fun getProducts(@Path("order_id") order_id: Int): Call<List<Product>>
 
-    /*@GET("getmovies")
-    fun getMovies(): Call<List<Movie>>
+    // 3 Endpoints to validate a delivery
+    @POST("order/update/{order_id}/{barcode}")
+    fun updateOrder(
+        @Path("order_id") order_id: String,
+        @Path("barcode") barcode: String
+    ): Call<String>
 
-    @GET("getactors")
-    fun getActors(): Call<List<Actor>>
+    // 4 Enpoints of the Dashboard
+    @GET("getTodayOrdersCount/{user_id}")
+    fun getTodayOrdersCount(@Path("user_id") user_id: Int): Call<Int>
 
-    @POST("postactor")
-    fun postActor(
-        @Body actor: Actor
-    ): Call<String>*/
+    @GET("getTodaysDeliveriesCount/{user_id}")
+    fun getTodaysDeliveriesCount(@Path("user_id") user_id: Int): Call<Int>
+
+    @GET("getDelayedOrdersCount/{user_id}")
+    fun getDelayedOrdersCount(@Path("user_id") user_id: Int): Call<Int>
 }
