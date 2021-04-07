92b487bc5236edb684e9d0595e48e2f7b9cc2e16
MajidMpressive <bouikkenmajid@gmail.com>
2021-04-07 09:19:28 +0100

    Google maps API

--- app/src/main/java/com/example/deliveryassistant/view/MapFragment.kt
+++ app/src/main/java/com/example/deliveryassistant/view/MapFragment.kt
@@ -1,11 +1,18 @@
 package com.example.deliveryassistant.view
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.android.volley.toolbox.StringRequest
 import com.example.deliveryassistant.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -14,13 +21,110 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import org.json.JSONObject
+
+import android.content.Context
+import android.graphics.Color
+
+import android.util.Log
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.Volley
+
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
 
 class MapFragment : Fragment() {
+    private lateinit var mMap: GoogleMap
+    private val REQUEST_LOCATION_PERMISSION = 1
+    private fun isPermissionGranted() : Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            mMap.isMyLocationEnabled = true
+        }
+        else {
+            ActivityCompat.requestPermissions(
+                activity as Activity,
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
 
     private val callback = OnMapReadyCallback { googleMap ->
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(11f))
+
+        /*val latitude = 36.74107390905088
+        //35.71413189162127
+        val longitude = 5.545641569204229
+        val destination = LatLng(latitude!!, longitude!!)
+        val zoomLevel = 15F
+
+
+        val lat = 35.74107390905088
+        val lng = 4.545641569204229
+        val origine=LatLng(lat,lng)
+        mMap.addMarker(MarkerOptions().position(destination).title("commande place"))
+        mMap.addMarker(MarkerOptions().position(origine).title(("my location")))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination,zoomLevel))
+        enableMyLocation()
+        enableMyLocation()
+        val path: MutableList<List<LatLng>> = ArrayList()
+        val urlDirections = "https://maps.googleapis.com/maps/api/directions/json?origin="+lat.toString()+","+lng.toString()+"&destination="+latitude.toString()+","+longitude.toString()+"&key="+getString(R.string.google_maps_key)
+
+        /*mLastLocation = location
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker!!.remove()
+        }*/
+        //Place current location marker
+        /*val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        markerOptions.title("Current Position")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        mCurrLocationMarker = mMap!!.addMarker(markerOptions)*/
+        val directionsRequest = object : StringRequest(Request.Method.GET, urlDirections, Response.Listener<String> {
+                response ->
+            val jsonResponse = JSONObject(response)
+            // Get routes
+            val routes = jsonResponse.getJSONArray("routes")
+            val legs = routes.getJSONObject(0).getJSONArray("legs")
+            val steps = legs.getJSONObject(0).getJSONArray("steps")
+            for (i in 0 until steps.length()) {
+                val points = steps.getJSONObject(i).getJSONObject("polyline").getString("points")
+                path.add(PolyUtil.decode(points))
+            }
+            for (i in 0 until path.size) {
+                this.mMap!!.addPolyline(PolylineOptions().addAll(path[i]).color(Color.RED))
+            }
+            var Duration=legs.getJSONObject(0).getJSONObject("duration").getString("text")
+            var Distance=legs.getJSONObject(0).getJSONObject("distance").getString("text")
+            Log.d("erp",Duration+Distance)
+            val disdur ="Temps estimé: "+Duration+"\nDistance estimé: "+Distance
+        }, Response.ErrorListener {
+                _ ->
+        }){}
+        val requestQueue = Volley.newRequestQueue(activity as Context)
+        requestQueue.add(directionsRequest)*/
     }
 
     override fun onCreateView(
@@ -37,4 +141,3 @@ class MapFragment : Fragment() {
         mapFragment?.getMapAsync(callback)
     }
 }
-
